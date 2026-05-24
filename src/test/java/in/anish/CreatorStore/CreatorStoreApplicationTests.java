42f173438e60c8fcd3ab693fdb2577e41cb27795
Anish <anish2003singh@gmail.com>
2026-05-24 18:39:43 +0530

    store changes

---
+++ src/test/java/in/anish/CreatorStore/CreatorStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package in.anish.CreatorStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreatorStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
