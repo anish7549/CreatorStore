527d8dd039de36b6422fe5819e40d7f3248f9756
Anish <anish2003singh@gmail.com>
2026-05-24 18:44:50 +0530

    New code added

--- src/main/java/in/anish/CreatorStore/CreatorStoreApplication.java
+++ src/main/java/in/anish/CreatorStore/CreatorStoreApplication.java
@@ -8,6 +8,7 @@ public class CreatorStoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CreatorStoreApplication.class, args);
+		System.out.println("Hello Anish");
 	}
 
 }
